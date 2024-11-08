2da73c08f8feb28cc3e2c7ba057409d7b8266eb1
Guilherme Paiva <guilhermepaiva9@gmail.com>
2024-11-08 15:19:54 -0300

    adds index update

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
 import authRoutes from "./routes/auth.route.js";
 import songRoutes from "./routes/song.route.js";
+import albumRoutes from "./routes/album.route.js";
+import statRoutes from "./routes/stat.route.js";
 
 dotenv.config();
 
